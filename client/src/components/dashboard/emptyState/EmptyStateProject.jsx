512c6410e61c7509b9aa2785c5993050f212bba6
devsid <yesid.manjarres@aule.co>
2022-06-15 15:06:37 -0500

    olha essa garota

--- client/src/components/dashboard/emptyState/EmptyStateProject.jsx
+++ client/src/components/dashboard/emptyState/EmptyStateProject.jsx
@@ -6,7 +6,7 @@ function EmptyStateProject() {
     return (
       <div className="flex-1 flex flex-column justify-center align-center">
         <img src={EmptySvg} alt="Empty" className="Image" width={250} />
-        <h1 className="mt-30 text-center" style={{ maxWidth: 400 }}>
+        <h1 className="mt-30 Title-Text text-center" style={{ maxWidth: 400 }}>
           Whoops! You don't have tasks yet. Start creating lists.
         </h1>
       </div>
--- client/src/components/dashboard/emptyState/EmptyStateTodos.jsx
+++ client/src/components/dashboard/emptyState/EmptyStateTodos.jsx
@@ -6,13 +6,13 @@ function EmptyStateTodos() {
     return (
       <div className="text-center" style={{
         position: 'absolute',
-        top: '25%',
+        top: '30%',
         right: '0',
         left: '0'
       }}
       >
-        <img src={EmptyTodos} width={250} alt="Empty todos" />
-        <h1 className="mt-10">You don't have todos. Start typing one above.</h1>
+        <img src={EmptyTodos} className="Image" width={200} alt="Empty todos" />
+        <h1 className="Title-Text mt-10">You don't have todos. Start typing one above.</h1>
       </div>
     );
   };
