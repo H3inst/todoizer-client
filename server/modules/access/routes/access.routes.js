92a24b18d6fb812cead2ced0d76eff73dc756a82
siddev <ewright.develop@gmail.com>
2022-07-10 13:47:06 -0500

    JOA

--- server/modules/access/routes/access.routes.js
+++ server/modules/access/routes/access.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const Access = require('../controllers/access.controllers');
+const AccessController = require('../controllers/access.controllers');
 const validateJsonWebToken = require('../../../middlewares/validate.jwt');
 
 const router = express.Router();
 
-router.post('/register', Access.registerUserController);
-router.post('/', Access.loginUserController);
-router.get('/', validateJsonWebToken, Access.validateTokenController);
+router.post('/register', AccessController.registerUser);
+router.post('/', AccessController.loginUser);
+router.get('/', validateJsonWebToken, AccessController.validateToken);
 
 module.exports = router;
